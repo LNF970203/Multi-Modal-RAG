ddbf7479dc5c9bff8d3517b0a50b97b003b60000
Lachin Fernando <77132441+LNF970203@users.noreply.github.com>
2023-12-28 12:29:10 +0530

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 # title
 st.title("Multi-Modal RAG Pipeline")
 # set the pipleine
-st.image("multimodalRAG.png", caption="Multi-Modal-RAG-Pipeline")
+st.image("app/multimodalRAG.png", caption="Multi-Modal-RAG-Pipeline")
 # project details
 st.subheader("Project is Based on...")
 st.markdown("- Backyard Birds Photo Gallery")
